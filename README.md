ba2cc505ce08edb2707833afadcfa9fab747b5d0
AlaaArar498 <alaaarar498@gmail.com>
2020-12-23 01:52:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# alaa-number-counter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/alaa-number-counter)
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  const [counter, setCounter] = React.useState(
+    Number(localStorage.getItem("counter") || 0)
+  );
+  const [numGap, setNumGap] = React.useState(Number(1));
+
+  const gapHandler = event => {
+    setNumGap(Number(event.target.value));
+  };
+  const clickHandler = event => {
+    event.target.name == "increase"
+      ? setCounter(counter + numGap)
+      : counter > 0 && counter - numGap >= 0
+      ? setCounter(counter - numGap)
+      : setCounter(0);
+  };
+  localStorage.setItem("counter", counter);
+
+  return (
+    <div>
+      <h1>Counter Bilgisi : {counter}</h1>
+      <button name="increase" onClick={clickHandler}>
+        arttir
+      </button>
+      <button name="decrease" onClick={clickHandler}>
+        azalt
+      </button>
+      <input
+        type="number"
+        name="gap"
+        style={{ width: 50, marginLeft: 15 }}
+        min="1"
+        onChange={gapHandler}
+      />
+    </div>
+  );
+}
